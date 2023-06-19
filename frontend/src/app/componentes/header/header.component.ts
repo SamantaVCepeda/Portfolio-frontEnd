a96aadb9bdaacea8c4d6f5b4eefef027c456517f
SamantaVCepeda <samantavcepeda@gmail.com>
2023-06-19 19:09:37 -0300

    Responsive

--- frontend/src/app/componentes/header/header.component.ts
+++ frontend/src/app/componentes/header/header.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
-}
+  mostrarOcultarMenu() {
+    let menuVisible: boolean = false;
+    const navElement = document.getElementById("nav");
+  
+    if (navElement) {
+      if (menuVisible) {
+        navElement.classList.remove("responsive");
+        menuVisible = false;
+      } else {
+        navElement.classList.add("responsive");
+        menuVisible = true;
+      }
+    }
+  }
+
+
+  // Funcion que oculta el manu una vez que es seleccionada una opcion
+
+  seleccionar(){
+    let menuVisible: boolean = false;
+    const navElement : HTMLElement | null = document.getElementById("nav");
+
+    navElement?.classList.remove("responsive");
+    menuVisible = false;
+  }
+
+
+
+
+
+
+
+} 
+
+
+
+
